1e014ea381a723d7b177b121486d50ddab1d99d0
DIANAHERNANDEZ <dianaitvo@gmail.com>
2025-09-02 13:09:04 -0600

    nueva linea

---
+++ Desktop/holaMundo/app.js
@@ -0,0 +1,23 @@
+let arraypets = ["pupy", "dog"]
+let arraypets2 = ["pupy", "dog"]
+console.log("HOLA MUNDO");
+let numero1 = 10;
+let numero2 = 5;
+
+
+let suma = numero1 + numero2;
+console.log("Suma:", suma);
+
+let resta = numero1 - numero2;
+console.log("Resta:", resta);
+
+let multiplicacion = numero1 * numero2;
+console.log("Multiplicación:", multiplicacion);
+
+let division = numero1 / numero2;
+console.log("División:", division);
+
+let modulo = numero1 % numero2;
+console.log(modulo);
+// soy un nuevo comentario 
+
---
+++ Desktop/holaMundo/holaMundo.py
@@ -0,0 +1,16 @@
+let numero1 = 10;
+let numero2 = 5;
+let numero3 = 2;
+
+
+let suma = numero1 + numero3;
+console.log("Suma:", suma);
+
+let resta = numero1 - numero2;
+console.log("Resta:", resta);
+
+let multiplicacion = numero1 * numero2;
+console.log("Multiplicación:", multiplicacion);
+
+let division = numero1 / numero2;
+console.log("División:", division);
