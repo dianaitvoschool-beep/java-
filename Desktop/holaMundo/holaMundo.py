68c31ff7ec0204245b3e2d147f509e65c0bd34fd
DIANAHERNANDEZ <dianaitvo@gmail.com>
2025-09-02 13:23:08 -0600

    Revert "nueva linea"
    This reverts commit 709582bb7cd0652fd59859af310ccb0389a911ff.

--- Desktop/holaMundo/holaMundo.py
+++ Desktop/holaMundo/holaMundo.py
@@ -1,11 +1,9 @@
-let arraypets = ["pupy", "dog"]
-let arraypets2 = ["pupy", "dog"]
-console.log("HOLA MUNDO");
 let numero1 = 10;
 let numero2 = 5;
+let numero3 = 2;
 
 
-let suma = numero1 + numero2;
+let suma = numero1 + numero3;
 console.log("Suma:", suma);
 
 let resta = numero1 - numero2;
@@ -16,8 +14,3 @@ console.log("Multiplicación:", multiplicacion);
 
 let division = numero1 / numero2;
 console.log("División:", division);
-
-let modulo = numero1 % numero2;
-console.log(modulo);
-// soy un nuevo comentario 
-
